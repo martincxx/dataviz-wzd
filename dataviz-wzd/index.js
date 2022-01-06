c25665f9e8b0def83bfe67e1be48340c8ea2974a
Martin Coronel <martincxx@gmail.com>
2022-01-06 13:20:26 +0300

    fix typo

--- dataviz-wzd/index.js
+++ dataviz-wzd/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { arc } from 'd3';
+import React from "react";
+import ReactDOM from "react-dom";
+import { arc } from "d3";
 
 const width = 960;
 const height = 500;
@@ -9,19 +9,15 @@ const circleY = height / 2;
 const circleRadius = 30;
 
 const handleMouseMove = (event) => {
- const {clientX, clientY} = event;
+  const { clientX, clientY } = event;
   console.log(clientX, clientY);
 };
 
 const App = () => (
-  <svg
-    width={width}
-    height={height}
-    onMouseMode={handleMouseMove}
-  >
+  <svg width={width} height={height} onMouseMove={handleMouseMove}>
     <circle cx={circleX} cy={circleY} r={circleRadius} />
   </svg>
 );
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
